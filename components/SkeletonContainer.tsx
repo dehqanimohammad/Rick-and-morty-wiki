bc493767a1bdbf1507785227c7f9e55f56b57306
dehqanimohammad <dehqaniimohamad@gmail.com>
2024-07-08 23:55:54 +0330

    fixin skeleton

--- components/SkeletonContainer.tsx
+++ components/SkeletonContainer.tsx
@@ -1,28 +1,20 @@
 import { Skeleton } from "@mui/material";
 import React from "react";
 
+const arr = [1, 2, 3];
+
 function SkeletonContainer() {
   return (
     <>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 mx-auto">
-        <Skeleton
-          sx={{ bgcolor: "grey.900" }}
-          variant="rectangular"
-          width={600}
-          height={350}
-        />
-        <Skeleton
-          sx={{ bgcolor: "grey.900" }}
-          variant="rectangular"
-          width={600}
-          height={350}
-        />
-        <Skeleton
-          sx={{ bgcolor: "grey.900" }}
-          variant="rectangular"
-          width={600}
-          height={350}
-        />
+        {arr.map((skeleton) => (
+          <Skeleton
+            sx={{ bgcolor: "grey.900" }}
+            variant="rectangular"
+            width={600}
+            height={220}
+          />
+        ))}
       </div>
     </>
   );
